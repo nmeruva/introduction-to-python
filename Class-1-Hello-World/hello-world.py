242787903e235af80ea15f0b7ee18529f4b10bf2
Nishika <nishika@macbookpro.lan>
2025-06-05 19:37:11 -0400

    pushing my first commit

---
+++ Class-1-Hello-World/hello-world.py
@@ -0,0 +1,23 @@
+print("hello world")
+
+if 5>2:
+    print("water")
+
+if -9<3:
+    print("paper")
+    print("computer")
+
+print(["bell peppers", "tomato", "cucumbers"])
+
+print(55%11)
+print(2**3)
+print(2+2*10+3)
+
+my_puppies = 2
+print(my_puppies)
+
+my_puppies = ["umbrella", "grass", "park", "my puppies"]
+print(my_puppies)
+
+
+
